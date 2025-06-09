821f5339e0361f9fc70db036c7e9c4b894099e00
vlad-gal <vlgalathevich@gmail.com>
2025-06-09 23:30:02 +0300

    change start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,27 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-uvicorn app:app --host 0.0.0.0 --port 80
+
+APP_DIR="/opt/myapp/aws-backend"
+VENV_DIR="$APP_DIR/venv"
+USER="root"
+
+cat <<EOF > /etc/systemd/system/backend.service
+[Unit]
+Description=backend
+After=network.target
+
+[Service]
+User=$USER
+WorkingDirectory=$APP_DIR
+ExecStart=$VENV_DIR/bin/uvicorn app:app --host 0.0.0.0 --port 80
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable backend
+systemctl restart backend
+systemctl status backend
